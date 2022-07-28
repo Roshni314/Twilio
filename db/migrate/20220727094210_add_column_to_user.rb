e3a6a7b1c94ea1438dbeb93ed499a1ea37089faa
Roshni314 <roshnipvarsani@gmail.com>
2022-07-28 11:15:20 +0530

    database files

---
+++ db/migrate/20220727094210_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :phone_number, :integer
+    add_column :users, :country_code, :string
+    add_index :users, :phone_number, unique: true
+  end
+end
